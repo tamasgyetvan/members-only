b4a03330919fc318966f8a6faffad01ed39b0df0
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-02 13:10:40 +0200

    Add mongoDB connectioN

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_CONNECTION_STRING;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
-var app = express();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
